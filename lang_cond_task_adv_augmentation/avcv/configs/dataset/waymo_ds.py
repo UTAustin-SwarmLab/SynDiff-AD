e6f8422c4972ca370d00e0550002c106de641d85
harshg99 <hgoel1000p@gmail.com>
2023-11-14 14:52:22 -0600

    mask2former training configs

--- lang_cond_task_adv_augmentation/avcv/configs/dataset/waymo_ds.py
+++ lang_cond_task_adv_augmentation/avcv/configs/dataset/waymo_ds.py
@@ -2,19 +2,19 @@
 dataset_type = 'WaymoDatasetMM'
 data_root = ''
 
-crop_size = (512, 512 )
+crop_size = (960, 640)
 num_classes = 29
 train_pipeline = [
     dict(type='AVResize', scale=crop_size, keep_ratio=False),
     dict(type='PackSegInputs')
 ]
 test_pipeline = [
-    dict(type='AVResize', scale=crop_size, keep_ratio=False),
+    dict(type='AVResize', scale=crop_size, keep_ratio=False, test=True),
     dict(type='PackSegInputs')
 ]
 train_dataloader = dict(
-    batch_size=2,
-    num_workers=1,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
     dataset=dict(
@@ -28,12 +28,12 @@ train_dataloader = dict(
         validation=False,
         segmentation=True,
         image_meta_data=True,
-        serialize_data=False
+        serialize_data=True
         ))
 
 val_dataloader = dict(
-    batch_size=1,
-    num_workers=1,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
@@ -43,11 +43,11 @@ val_dataloader = dict(
             EVAL_DIR = '/store/datasets/waymo/validation/',
             TEST_SET_SOURCE = '/content/waymo-open-dataset/tutorial/2d_pvps_validation_frames.txt',
             SAVE_FRAMES = [0,1,2]),
-        pipeline=train_pipeline,
+        pipeline=test_pipeline,
         validation=True,
         segmentation=True,
         image_meta_data=True,
-        serialize_data=False
+        serialize_data=True
         ))
 test_dataloader = val_dataloader
 val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
