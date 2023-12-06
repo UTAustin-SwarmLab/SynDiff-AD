abf0a5cd08ee7dabdce0fe02684faa62cbea499d
harshg99 <hgoel1000p@gmail.com>
2023-12-06 17:13:48 -0600

    configs added for segformer training

--- lang_cond_task_adv_augmentation/avcv/configs/models/segformer_mit-b0_8xb2-160k_waymo-512x512.py
+++ lang_cond_task_adv_augmentation/avcv/configs/models/segformer_mit-b0_8xb2-160k_waymo-512x512.py
@@ -1,5 +1,14 @@
-_base_ = ['mask2former_r50_8xb2-90k_waymo-960x640.py']
+_base_ = [
+    '../_base_/models/segformer_mit-b0.py', '../dataset/waymo_ds.py',
+    '../default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
 crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b0_20220624-7e0fe6dd.pth'  # noqa
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(init_cfg=dict(type='Pretrained', checkpoint=checkpoint)),
+    decode_head=dict(num_classes=150))
 
 train_pipeline = [
     dict(type='AVResize', scale=crop_size, keep_ratio=False),
@@ -23,32 +32,46 @@ test_pipeline = [
                                           'condition',
                                           'reduce_zero_label'])
 ]
-data_preprocessor = dict(
-    type='SegDataPreProcessor',
-    mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375],
-    bgr_to_rgb=False,
-    pad_val=0,
-    seg_pad_val=255,
-    size=crop_size,
-    test_cfg=dict(size_divisor=32)
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
     )
+]
+
 
 train_dataloader = dict(
-    batch_size=8,
-    num_workers=8,
+    batch_size=3,
+    num_workers=3,
     dataset=dict(
         pipeline=train_pipeline,
         )
   )
 
 val_dataloader = dict(
-    batch_size=16,
-    num_workers=16,
+    batch_size=6,
+    num_workers=6,
         dataset=dict(
         pipeline=test_pipeline,
     )
    )
-
 test_dataloader = val_dataloader
-model = dict(data_preprocessor=data_preprocessor)
---
+++ lang_cond_task_adv_augmentation/avcv/configs/models/segformer_mit-b3_8xb2-160k_synthwaymo-512x512.py
@@ -0,0 +1,82 @@
+_base_ = [
+    '../_base_/models/segformer_mit-b0.py', '../dataset/synth_waymo_ds.py',
+    '../default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b3_20220624-13b1141c.pth'  # noqa
+
+train_pipeline = [
+    dict(type='AVResize', scale=crop_size, keep_ratio=False),
+    dict(type='PackSegInputs', meta_keys=['context_name',
+                                          'context_frame',
+                                          'camera_id',
+                                          'ori_shape',
+                                          'img_shape',
+                                          'scale_factor',
+                                          'condition',
+                                          'reduce_zero_label'])
+]
+test_pipeline = [
+    dict(type='AVResize', scale=crop_size, keep_ratio=False, test=True),
+    dict(type='PackSegInputs', meta_keys=['context_name',
+                                          'context_frame',
+                                          'camera_id',
+                                          'ori_shape',
+                                          'img_shape',
+                                          'scale_factor',
+                                          'condition',
+                                          'reduce_zero_label'])
+]
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
+    )
+]
+
+
+train_dataloader = dict(
+    batch_size=3,
+    num_workers=3,
+    dataset=dict(
+        pipeline=train_pipeline,
+        )
+  )
+
+val_dataloader = dict(
+    batch_size=6,
+    num_workers=6,
+        dataset=dict(
+        pipeline=test_pipeline,
+    )
+   )
+test_dataloader = val_dataloader
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(init_cfg=dict(type='Pretrained', checkpoint=checkpoint),
+                    embed_dims=64,
+                    num_heads=[1, 2, 5, 8],
+                    num_layers=[3, 4, 18, 3]),
+    decode_head=dict(num_classes=150,
+                     in_channels=[64, 128, 320, 512]))
