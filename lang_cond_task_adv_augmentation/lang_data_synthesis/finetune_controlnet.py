1f2ab70cfad4d44e058e908948225dd99d04f430
harshg99 <hgoel1000p@gmail.com>
2023-12-31 00:24:18 -0600

    full initial function for finetuning controlent

---
+++ lang_cond_task_adv_augmentation/lang_data_synthesis/finetune_controlnet.py
@@ -0,0 +1,84 @@
+import ControlNet.config
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(),'ControlNet/'))
+from ControlNet.cldm.hack import disable_verbosity, enable_sliced_attention
+
+
+disable_verbosity()
+
+if ControlNet.config.save_memory:
+    enable_sliced_attention()
+
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from lang_data_synthesis.dataset import AVControlNetDataset
+from ControlNet.cldm.logger import ImageLogger
+from ControlNet.cldm.model import create_model, load_state_dict
+
+from omegaconf import OmegaConf
+
+import torch
+import os
+
+class ControlNetFineTune:
+    
+    def __init__(self, config) -> None:
+        
+        self.config = config
+        if not os.path.exists(config.path_init_ckpt):
+            self.model = self.setup_checkpoint(config)
+        else:
+            self.model = create_model(config.config_path_sd15).cpu()
+            self.model.load_state_dict(load_state_dict(config.path_init_ckpt), 
+                                                    strict = False)
+
+            
+        self.model.learning_rate = config.learning_rate
+        self.model.sd_locked = config.sd_locked
+        self.model.only_mid_control = config.only_mid_control
+        
+
+    
+    def setup_checkpoint(self, 
+                         config):
+        
+        model = create_model(config.config_path_sd15).cpu()
+        model.load_state_dict(load_state_dict(config.path_sd15), 
+                                                strict = False)
+        model.load_state_dict(
+            load_state_dict(config.path_sd15_with_control),
+            strict = False)
+
+        torch.save(model.state_dict(), config.path_init_ckpt)
+        print('Transferred model saved at ' + config.path_init_ckpt)
+        return model
+    
+    def finetune_model(self):
+        # Misc
+        configs = {
+            'waymo' : OmegaConf.load('lang_data_synthesis/waymo_config.yaml'),
+            'bdd' : OmegaConf.load('lang_data_synthesis/bdd_config.yaml')
+        }
+    
+        # Switch
+        # Add wandb callback logger
+        # wandb_logger = pl.loggers.WandbLogger(project='controlnet', save_dir='lang_data_synthesis/finetune_logs/')
+        # wandb_logger.watch(self.model)
+        
+        
+        dataset = AVControlNetDataset(configs)
+        dataloader = DataLoader(dataset, num_workers=4, batch_size=self.config.batch_size, shuffle=True)
+        logger = ImageLogger(batch_frequency=self.config.logger_freq)
+        trainer = pl.Trainer(gpus=6, precision=32, callbacks=[logger])#, logger= wandb_logger)
+
+
+        # Train!
+        trainer.fit(self.model, dataloader)
+
+
+if  __name__ == "__main__":
+    config = OmegaConf.load('lang_data_synthesis/finetune_config.yaml')
+    finetune = ControlNetFineTune(config)
+    finetune.finetune_model()
