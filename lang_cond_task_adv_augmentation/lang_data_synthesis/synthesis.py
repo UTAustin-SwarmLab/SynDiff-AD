49f1d02bcedde66fa4d1ae1d347006752da84b69
harshg99 <hgoel1000p@gmail.com>
2024-03-01 23:44:36 -0600

    synthetic data gen

--- lang_cond_task_adv_augmentation/lang_data_synthesis/synthesis.py
+++ lang_cond_task_adv_augmentation/lang_data_synthesis/synthesis.py
@@ -280,7 +280,7 @@ class SyntheticAVGenerator:
                 ]['condition'].values[0]
             elif self.dataset_type == "bdd":
                 prompt = self.prompt_df.loc[
-                    (img_data['file_name'] == self.prompt_df['context_name'])
+                    (img_data['file_name'] == self.prompt_df['file_name'])
                 ]['caption'].values[0]
                 source_condition = self.metadata_conditions.loc[
                     (img_data['file_name'] == self.metadata_conditions['context_name'])
